2bce6307e5a5ff62dd42f4bd174db810049eab03
Jeremy Salinas (TS-NA) <jeremy_salinas@trendmicro.com>
2024-02-15 10:43:25 -0600

    first commit, testing phase

---
+++ main.py
@@ -0,0 +1,64 @@
+import boto3
+from botocore.exceptions import ClientError
+
+ec2 = boto3.resource('ec2', region_name='us-west-2')
+userInstance = boto3.client('ec2')
+ssm = boto3.client('ssm')
+ami = ''
+# get user platform
+def get_platform():
+    userInput = input('Windows or Linux? ').lower()
+    platform = userInput if userInput == 'windows' or userInput == 'linux' else ''
+    return platform or 'linux'
+
+platform = get_platform()
+
+# get latest recommended ami
+if platform.lower() == 'linux':
+    ami = ssm.get_parameter(Name='/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2')['Parameter']['Value']
+else:
+    ami = ssm.get_parameter(Name='/aws/service/ami-windows-latest/Windows_Server-2016-English-Full-Base')['Parameter']['Value']
+
+# check for duplicate group name
+def check_sec_group(groupName):
+    # userInstance.describe_vpcs(Filters=[{'Name': 'is-default','Values': ['true']}])['Vpcs'][0]['VpcId']
+    secGroups = userInstance.describe_security_groups()['SecurityGroups']
+    secGroupNames = [group['GroupName'] for group in secGroups]
+    secGroupExists = groupName in secGroupNames
+    return secGroupExists
+
+
+# create security group
+def create_sec_group():
+    secGroupName = input('Enter name for new security group: ')
+    validName = check_sec_group(secGroupName)
+    if not validName:
+        instanceSecGroup = userInstance.create_security_group(
+            Description='Created from ec2 cli',
+            GroupName=secGroupName
+        )['GroupId']
+        return instanceSecGroup
+    print("You've entered an existing security group name.")
+    exit
+
+instanceSecGroup = create_sec_group()
+# create keypair and save
+
+#  create instance returns instance object
+instance = ec2.create_instances(ImageId=ami,InstanceType='t2.medium',KeyName='JeremyS-USWest2-KP',SecurityGroupIds=[instanceSecGroup],MaxCount=1,MinCount=1)[0]
+instance.wait_until_running()
+
+# enable ssh/rdp depending on platform
+instanceInfo = userInstance.describe_instances(InstanceIds=[instance.id])['Reservations'][0]['Instances'][0]
+
+
+userInstance.authorize_security_group_ingress(
+    GroupId=instanceSecGroup,
+    CidrIp='0.0.0.0/0',
+    FromPort=22,
+    IpProtocol='tcp',
+    ToPort=22
+)
+
+# enable ssh/rdp depending on platform
+print(f"ID: {instance.id}\nDNS: {instanceInfo['PublicDnsName']}\nPublic IP: {instanceInfo['PublicIpAddress']}\nKeyName: {instanceInfo['KeyName']}")
