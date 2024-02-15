cc9993aff035c99a8f32250455bcf29021227999
Jeremy Salinas <jeremysalinas@Jeremys-Air.lan>
2024-02-15 15:18:58 -0600

    Added instance deletion on termination and function to delete SG.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import boto3
+import boto3, requests, json
 from botocore.exceptions import ClientError
 
 ec2 = boto3.resource('ec2', region_name='us-west-2')
 userInstance = boto3.client('ec2')
 ssm = boto3.client('ssm')
+publicIp = requests.get('https://api.ipify.org?format=json').json()['ip']
 ami = ''
 # get user platform
+# TODO: display a list of windows/linux OS and have user select before loading ami
 def get_platform():
     userInput = input('Windows or Linux? ').lower()
     platform = userInput if userInput == 'windows' or userInput == 'linux' else ''
@@ -41,20 +43,41 @@ def create_sec_group():
     print("You've entered an existing security group name.")
     exit
 
+# delete security group
+def delete_sec_group(secGroupId):
+    userInstance.delete_security_group(
+        GroupId=secGroupId
+    )
+    return f'{secGroupId} deleted'
+
+# delete instance
+def delete_instance(instanceId):
+    userInstance.terminate_instances(
+        InstanceIds=[
+            instanceId
+        ]
+    )
+    return f'{instanceId} deleted'
+
 instanceSecGroup = create_sec_group()
 # create keypair and save
 
 #  create instance returns instance object
-instance = ec2.create_instances(ImageId=ami,InstanceType='t2.medium',KeyName='JeremyS-USWest2-KP',SecurityGroupIds=[instanceSecGroup],MaxCount=1,MinCount=1)[0]
+instance = ec2.create_instances(
+    ImageId=ami,
+    InstanceType='t2.medium',
+    KeyName='JeremyS-USWest2-KP',
+    SecurityGroupIds=[instanceSecGroup],
+    InstanceInitiatedShutdownBehavior='terminate',
+    MaxCount=1,MinCount=1)[0]
 instance.wait_until_running()
 
 # enable ssh/rdp depending on platform
 instanceInfo = userInstance.describe_instances(InstanceIds=[instance.id])['Reservations'][0]['Instances'][0]
 
-
 userInstance.authorize_security_group_ingress(
     GroupId=instanceSecGroup,
-    CidrIp='0.0.0.0/0',
+    CidrIp=f'{publicIp}/32',
     FromPort=22,
     IpProtocol='tcp',
     ToPort=22
